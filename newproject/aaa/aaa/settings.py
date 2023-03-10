ef4db77c9f5617e98817544fd30adc7c73f3ccfa
Andrew <102332589+Sehemhet@users.noreply.github.com>
2023-03-10 09:29:17 +0200

    delete key

--- newproject/aaa/aaa/settings.py
+++ newproject/aaa/aaa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d3+9+%k6e_!nftquldg#qh69fj=s^^u*kmmby4t6v0$w=1+g13'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
